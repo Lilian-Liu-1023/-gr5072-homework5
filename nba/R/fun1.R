a8ff5f4ca1b2d57d01a5a61969c181c012f8d9f5
Xiaofan Liu <xl3042@columbia.edu>
2022-03-24 15:56:23 -0400

    nba

---
+++ nba/R/fun1.R
@@ -0,0 +1,16 @@
+#' Function 1
+#'
+#' This function allows you to get the result of the highest points of a player in a specific year
+#' @param year
+#' @keywords highest points
+#' @export
+#' @examples
+#' fun1()
+
+fun1 <- function(year){
+  d1 <- d %>%
+    filter(Year == year)
+  output <- d1 %>%
+    filter(PTS == max(d1$PTS))
+  output
+}
